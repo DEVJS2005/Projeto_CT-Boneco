9a2a77a1e0c4b7f69f9a03c50c67fa87572da5d1
DEVJS2005 <jvl14533@outlook.com>
2023-03-29 20:28:27 -0300

    initial
    Primeiro comit do projeto

---
+++ Projeto CT/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Projeto_CT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto_CT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public bool VerificarLogin()
+        {
+            if (Session["UserAuth"] != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        BDEntities bd = new BDEntities();
+        public ActionResult Index()
+        {
+            if(VerificarLogin()){
+                return View((usuario)Session["UserAuth"]);
+            }
+            else
+            {
+                return RedirectToAction("telaLogin","Login");
+            }
+           
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Projeto CT/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Projeto_CT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto_CT.Controllers
+{
+    public class LoginController : Controller
+    {
+        BDEntities bd = new BDEntities();
+        // GET: telaLogin
+        public ActionResult telaLogin()
+        {
+            return View();
+        }
+        public ActionResult login(string usuarioL, string senhaL)
+        {
+            usuario user = bd.usuario.ToList().Find(x => x.nome.Equals(usuarioL));
+            if (user == null)
+            {
+                return View(ViewBag.text = "Usuário inexistente");
+            }
+            if (usuarioL == user.nome && senhaL == user.senha)
+            {
+                Session["UserAuth"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public ActionResult logout()
+        {
+            Session["UserAuth"] = null;
+            return RedirectToAction("telaLogin", "Login");
+        }
+    }
+}
